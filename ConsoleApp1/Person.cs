203f13bd15676679d8d075035e3389872bc87a8d
Miroslawv <vajtehovicmiroslav@gmail.com>
2025-12-01 08:32:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        String name;
+        String surname;
+        DateTime date;
+
+        public Person(string name, string surname, DateTime date)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.date = date;
+        }
+
+        public Person()
+        {
+            this.name = "Имя";
+            this.surname = "Фамилия";
+            this.date= DateTime.Now;
+        }
+
+        public String Name
+        {
+            get 
+            { 
+                return this.name; 
+            } 
+            set 
+            {
+                this.name = value; 
+            }
+        }
+
+        public String Surname
+        {
+            get
+            {
+                return this.surname;
+            }
+            set
+            {
+                this.surname = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.date;
+            }
+            set
+            {
+                this.date = value;
+            }
+        }
+
+
+    }
+}
