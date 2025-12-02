fb172b574b86952c9b56b0e9850dccec5e9f0ad8
ArtyomAlishevich <ArtjomAlishevich@gmail.com>
2025-12-02 21:27:17 +0300

    Update Program.cs
    add my part

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -43,3 +43,89 @@ double end3 = Environment.TickCount;
 Console.WriteLine($"Время обработки одномерного массива: {end1 - start1}");
 Console.WriteLine($"Время обработки двумерного массива: {end2 - start2}");
 Console.WriteLine($"Время обработки ступенчатого массива: {end3 - start3}");
+
+
+
+Student st = new Student();
+Console.WriteLine(st.ToShortString());
+Console.WriteLine(st[Education.Specialist]);
+Console.WriteLine(st[Education.Bachelor]);
+Console.WriteLine(st[Education.SecondEducation]);
+st.Pers = new Person("Ажлорф", "Ашпеп", new DateTime(2008, 8, 14));
+st.Educ = Education.Bachelor;
+st.NumGroup = 666;
+st.Exams = new Exam[]
+{
+    new Exam("№1", 5, new DateTime(2025, 12, 1)),
+    new Exam("№1", 6, new DateTime(2026, 1, 1))
+};
+Console.WriteLine(st);
+st.AddExam(new Exam("#3", 7, new DateTime(2025, 2, 1)), new Exam("#4", 8, new DateTime(2025, 3, 1)), new Exam("#5", 10, new DateTime(2025, 4, 1)));
+Console.WriteLine(st);
+
+nrow = 0;
+ncolumn = 0;
+do { nrow = setIntValueByUser("Введите количество строк в массивах: "); } while (nrow < 1);
+do { nrow = setIntValueByUser("Введите столбцов строк в массивах: "); } while (ncolumn < 1);
+
+Exam[] arr1 = new Exam[nrow * ncolumn];
+Exam[,] arr2 = new Exam[nrow, ncolumn];
+Exam[][] arr3 = new Exam[nrow][];
+for(int i = 0; i < nrow; i++)
+{
+    arr3[i] = new Exam[ncolumn];
+}
+
+double s1 = Environment.TickCount;
+for (int i = 0; i < nrow * ncolumn; i++)
+{
+    arr1[i] = new Exam();
+}
+double e1 = Environment.TickCount;
+
+double s2 = Environment.TickCount;
+for (int i = 0; i < nrow; i++)
+{
+    for (int j = 0; j < ncolumn; j++)
+    {
+        arr2[i, j] = new Exam();
+    }
+}
+double e2 = Environment.TickCount;
+
+double s3 = Environment.TickCount;
+for (int i = 0; i < nrow; i++)
+{
+    for (int j = 0; j < ncolumn; j++)
+    {
+        arr3[i][j] = new Exam();
+    }
+}
+double e3 = Environment.TickCount;
+
+Console.WriteLine($"Время обработки одномерного массива: {e1 - s1}");
+Console.WriteLine($"Время обработки двумерного массива: {e2 - s2}");
+Console.WriteLine($"Время обработки ступенчатого массива: {e3 - s3}");
+
+static int setIntValueByUser(string line = "")
+{
+    try
+    {
+        Console.Write(line);
+        int n = Convert.ToInt32(Console.ReadLine());
+        return n;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ошибка. Введено некорректное значение. Необходимо число. Будет возвращён 0.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Ошибка. Введено некоректное значение. Нужно меньшее число. Будет возвращён 0.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+    return 0;
+}
